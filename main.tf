6dbbc066e02afa4b7138131f47e489c04493424d
ravooru12 <ravoorus12@gmail.com>
2023-08-06 13:25:25 +0530

    vela team terraform

--- main.tf
+++ main.tf
@@ -7,13 +7,27 @@ resource "aws_subnet" "my_subnet" {
   cidr_block = "10.0.0.0/24"
 }
 
-resource "aws_instance" "my_instance" {
-  ami           = var.ami_id  # Replace with a valid AMI ID
-  instance_type = var.inst_type
-  subnet_id     = aws_subnet.my_subnet.id
+#resource "aws_instance" "my_instance" {
+#ami           = var.ami_id  # Replace with a valid AMI ID
+#instance_type = var.inst_type
+#subnet_id     = aws_subnet.my_subnet.id
 
-  tags = {
-    Name = "terraEC2Instance"
-  }
+#tags = {
+#Name = "terraEC2Instance"
+#}
+#}
+
+resource "aws_internet_gateway" "devops-igw" {
+  vpc_id = aws_vpc.my_vpc.id
 }
 
+resource "aws_route_table" "Devopsroute" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    //associated subnet can reach everywhere
+    cidr_block = "0.0.0.0/0"
+    //CRT uses this IGW to reach internet
+    gateway_id = aws_internet_gateway.devops-igw.id
+  }
+}
