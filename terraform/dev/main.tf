ebfade8d558711c0a4b79a3770aaf964785b64e0
root <root@DESKTOP-RDFI041>
2023-10-30 09:29:50 +0530

    new changes

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -1,70 +1,29 @@
-# vpc creation
 resource "aws_vpc" "demovpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr_block
   tags = {
     Name = "Demo VPC"
   }
 }
-
-# Creating 1st web subnet
-resource "aws_subnet" "public-subnet-1" {
-  vpc_id                  = aws_vpc.demovpc.id
-  cidr_block              = "10.0.0.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a"
-  tags = {
-    Name = "Web Subnet 1"
-  }
+resource "aws_subnet" "public-subnet" {
+  count                   = var.subnet_count
+  vpc_id                  = var.vpc_id
+  cidr_block              = var.subnet_cidr_blocks[count.index]
+  map_public_ip_on_launch = var.map_public_ip_on_launch
+  availability_zone       = var.availability_zone
+  tags                    = merge(var.subnet_tags, { Name = "${var.subnet_tags["Name"]} ${count.index + 1}" })
 }
-# Creating 2nd web subnet
-resource "aws_subnet" "public-subnet-2" {
-  vpc_id                  = aws_vpc.demovpc.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a"
-  tags = {
-    Name = "Web Subnet 2"
-  }
+resource "aws_subnet" "database-subnet" {
+  vpc_id            = var.vpc_id
+  cidr_block        = var.subnet_cidr_block
+  availability_zone = var.availability_zone
+  tags              = var.subnet_tags
 }
-
-# Creating 1st application subnet
-resource "aws_subnet" "application-subnet-1" {
-  vpc_id                  = aws_vpc.demovpc.id
-  cidr_block              = "10.0.2.0/24"
-  map_public_ip_on_launch = false
-  availability_zone       = "us-east-1a"
-  tags = {
-    Name = "Application Subnet 1"
-  }
-}
-# Creating 2nd application subnet
-resource "aws_subnet" "application-subnet-2" {
-  vpc_id                  = aws_vpc.demovpc.id
-  cidr_block              = "10.0.3.0/24"
-  map_public_ip_on_launch = false
-  availability_zone       = "us-east-1a"
-  tags = {
-    Name = "Application Subnet 2"
-  }
-}
-
-# Create Database Private Subnet
-resource "aws_subnet" "database-subnet-1" {
-  vpc_id            = aws_vpc.demovpc.id
-  cidr_block        = "10.0.4.0/24"
-  availability_zone = "us-east-1a"
-  tags = {
-    Name = "Database Subnet 1"
-  }
-}
-
-# Creating WEB EC2 instance
 resource "aws_instance" "WEBinstance" {
-  ami                         = "ami-0ecc890394f5c1234"
-  instance_type               = "t2.micro"
-  count                       = 2
-  key_name                    = "myKey"
-  associate_public_ip_address = true
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  count                       = var.instance_count
+  key_name                    = var.key_name
+  associate_public_ip_address = var.associate_public_ip
   tags = {
     Name = "WEB Instance"
   }
@@ -73,13 +32,12 @@ resource "aws_instance" "WEBinstance" {
   ]
 }
 
-# Creating APP EC2 instance
 resource "aws_instance" "APPinstance" {
-  ami                         = "ami-0ecc890394f5c1234"
-  instance_type               = "t2.micro"
-  count                       = 2
-  key_name                    = "myKey"
-  associate_public_ip_address = true
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  count                       = var.instance_count
+  key_name                    = var.key_name
+  associate_public_ip_address = var.associate_public_ip
   tags = {
     Name = "APP Instance"
   }
@@ -88,32 +46,29 @@ resource "aws_instance" "APPinstance" {
   ]
 }
 
-
-# creating DB EC2 instance
 provider "aws" {
   region = "us-east-1" # Replace with your desired AWS region
 }
 
 resource "aws_db_instance" "example_db" {
-  allocated_storage = 20
-  storage_type      = "gp2"
-  engine            = "mysql"
-  engine_version    = "5.7"
-  instance_class    = "db.t2.micro"
-  username          = "mydbuser"
-  password          = "mydbpassword"
-
-  parameter_group_name = "default.mysql5.7"
-  skip_final_snapshot  = true
-  tags = {
-    Name = "DB Instance"
-  }
+  allocated_storage = var.db_instance_params.allocated_storage
+  storage_type      = var.db_instance_params.storage_type
+  engine            = var.db_instance_params.engine
+  engine_version    = var.db_instance_params.engine_version
+  instance_class    = var.db_instance_params.instance_class
+  username          = var.db_instance_params.username
+  password          = var.db_instance_params.password
+  parameter_group_name = var.db_instance_params.parameter_group_name
+  skip_final_snapshot  = var.db_instance_params.skip_final_snapshot
+  tags = var.db_instance_params.tags
 }
 
+         " {
+  region = var.aws_region
+}
 
-# internet gateway creation
 resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.demovpc.id
+  vpc_id = var.vpc_id
 
   tags = {
     Name = "main"
@@ -121,29 +76,29 @@ resource "aws_internet_gateway" "gw" {
 }
 
 resource "tls_private_key" "pk" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
+  algorithm = var.tls_key_algorithm
+  rsa_bits  = var.tls_rsa_bits
 }
 
 resource "aws_key_pair" "kp" {
-  key_name   = "myKey" # Create "myKey" to AWS!!
+  key_name   = var.key_name
   public_key = tls_private_key.pk.public_key_openssh
 
-  provisioner "local-exec" { # Create "myKey.pem" to your computer!!
-    command = "echo '${tls_private_key.pk.private_key_pem}' > ./myKey.pem"
+  provisioner "local-exec" {
+    command = "echo '${tls_private_key.pk.private_key_pem}' > ./${var.key_name}.pem"
   }
 }
 
-# security group
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.demovpc.id
+  vpc_id      = var.vpc_id
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = var.cidr_blocks
   }
 }
+
