756875b4daffdda3a23cbd325e834f0de4988237
root <root@DESKTOP-RDFI041>
2023-11-10 10:11:29 +0530

    new commit

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -12,4 +12,29 @@ resource "aws_subnet" "public-subnet-1" {
   availability_zone       = var.public_subnet_az
   tags                    = var.public_subnet_tags
 }
+resource "aws_instance" "WEBinstance" {
+  ami                         = "ami-087c17d1fe0178315"
+  instance_type               = var.instance_type
+  count                       = var.instance_count
+  key_name                    = var.key_name
+  associate_public_ip_address = var.associate_public_ip
+  tags                        = var.instance_tags
+  depends_on = [
+    aws_key_pair.kp
+  ]
+}
+
+resource "tls_private_key" "pk" {
+  algorithm = "RSA"
+  rsa_bits  = var.rsa_bits
+}
+
+resource "aws_key_pair" "kp" {
+  key_name   = var.key_name
+  public_key = tls_private_key.pk.public_key_openssh
+
+  provisioner "local-exec" {
+    command = "echo '${tls_private_key.pk.private_key_pem}' > ./myKey.pem"
+  }
+}
 
--- terraform/dev/variables.tf
+++ terraform/dev/variables.tf
@@ -31,4 +31,35 @@ variable "public_subnet_tags" {
     Name = "Web Subnet 1"
   }
 }
+variable "instance_count" {
+  description = "Number of EC2 instances to create"
+  default     = 2
+}
 
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "Name of the AWS key pair"
+  default     = "myKey"
+}
+
+variable "associate_public_ip" {
+  description = "Whether to associate a public IP address with instances"
+  default     = true
+}
+
+variable "instance_tags" {
+  description = "Tags for the EC2 instances"
+  type        = map(string)
+  default     = {
+    Name = "WEB Instance"
+  }
+}
+
+variable "rsa_bits" {
+  description = "Number of bits in the RSA key"
+  default     = 4096
+}
