cfd5fbb64a5f2d4bf07a94470f2016ab6107db9d
root <root@DESKTOP-RDFI041>
2023-12-13 16:43:10 +0530

    update 13

--- terraform/dev/variables.tf
+++ terraform/dev/variables.tf
@@ -3,30 +3,10 @@ variable "vpc_cidr_block" {
   default = "10.0.0.0/16"
 }
 
-variable "public_subnet_cidr" {
-  description = "CIDR block for the public subnet"
-  type        = string
-  default     = "10.0.0.0/24"
-}
-
-variable "public_subnet_az" {
-  description = "Availability Zone for the public subnet"
-  type        = string
-  default     = "us-east-1a"
-}
-
 variable "vpc_tags" {
   description = "Tags for the VPC"
   type        = map(string)
   default     = {
     Name = "Demo VPC"
-  }
-}
-
-variable "public_subnet_tags" {
-  description = "Tags for the public subnet"
-  type        = map(string)
-  default     = {
-    Name = "Web Subnet 1"
   }
 }
