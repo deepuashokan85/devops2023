ebcac13841d2063e79b05b6434812da6a13f6683
root <root@DESKTOP-RDFI041>
2023-12-30 09:50:45 +0530

    new update 30

---
+++ terraform/modules/EC2/variables.tf
@@ -0,0 +1,11 @@
+
+variable "ami_id" {
+  type    = string
+  default = "ami-0230bd60aa48260c6" 
+}
+
+variable "inst_type" {
+  type    = string
+  default = "t2.micro" 
+}
+
--- terraform/modules/nat/main.tf
+++ terraform/modules/nat/main.tf
@@ -1,31 +1,32 @@
 resource "aws_eip" "nat1" {
-depends_on = [aws_internet_gateway.main]
+  depends_on = [aws_internet_gateway.main]
 }
 resource "aws_vpc" "main" {
-  cidr_block = "192.168.0.0/18"
+  cidr_block = "10.0.0.0/16"
   tags = {
     Name = "main"
   }
+}
 
 resource "aws_internet_gateway" "main" {
-vpc_id = aws_vpc.main.id
-tags = {
-name = "main"
+  vpc_id = aws_vpc.main.id
+  tags = {
+    name = "main"
+  }
 }
-}
-resource "aws_subnet" "public_1"{
-vpc_id = aws_vpc.main.id
-cidr_block= "192.168.0.0/18"
-availability_zone = "us-east-1"
-map_public_ip_on_lunch = true
-tags = {
-name = "main"
-}
+resource "aws_subnet" "public_1" {
+  vpc_id                 = aws_vpc.main.id
+  cidr_block             = "10.0.0.0/20"
+  availability_zone      = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    name = "main"
+  }
 }
 resource "aws_nat_gateway" "gw1" {
-allocation_id = aws_eip.nat1.id
-subnet_id = Aws_subnet.public_1.id
-tags = {
-name = "NAT 1"
-}
+  allocation_id = aws_eip.nat1.id
+  subnet_id     = aws_subnet.public_1.id
+  tags = {
+    name = "NAT 1"
+  }
 }
