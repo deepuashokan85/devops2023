29682008b516ad0231c175a3ea0b7b1e28f28715
root <root@DESKTOP-RDFI041>
2023-12-31 12:04:30 +0530

    new update31

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -11,30 +11,9 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_instance" "WEBinstance" {
-  ami                         = "ami-087c17d1fe0178315"
-  instance_type               = var.instance_type
-  count                       = var.instance_count
-  key_name                    = var.key_name
-  associate_public_ip_address = var.associate_public_ip
-  tags                        = var.instance_tags
-  depends_on = [
-    aws_key_pair.kp
-  ]
+# vpc creation
+resource "aws_vpc" "demovpc" {
+  cidr_block = var.vpc_cidr_block
+  tags      = var.vpc_tags
 }
-
-resource "tls_private_key" "pk" {
-  algorithm = "RSA"
-  rsa_bits  = var.rsa_bits
-}
-
-resource "aws_key_pair" "kp" {
-  key_name   = var.key_name
-  public_key = tls_private_key.pk.public_key_openssh
-
-  provisioner "local-exec" {
-    command = "echo '${tls_private_key.pk.private_key_pem}' > ./myKey.pem"
-  }
-}
-
 
