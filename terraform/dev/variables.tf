4269fe22dc6bbbbcdc3bddb35941ce17fb1fdc3b
root <root@DESKTOP-RDFI041>
2024-02-19 09:06:41 +0530

    new update19

--- terraform/dev/variables.tf
+++ terraform/dev/variables.tf
@@ -1,12 +1,34 @@
-variable "vpc_cidr_block" {
-  type    = string
-  default = "10.0.0.0/16"
+variable "instance_count" {
+  description = "Number of EC2 instances to create"
+  default     = 1
 }
-variable "vpc_tags" {
-  description = "Tags for the VPC"
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "Name of the AWS key pair"
+  default     = "myKey"
+}
+
+variable "associate_public_ip" {
+  description = "Whether to associate a public IP address with instances"
+  default     = true
+}
+
+variable "instance_tags" {
+  description = "Tags for the EC2 instances"
   type        = map(string)
   default     = {
-    Name = "Demo VPC"
+    Name = "WEB Instance"
   }
 }
+
+variable "rsa_bits" {
+  description = "Number of bits in the RSA key"
+  default     = 4096
+}
+
 
