14c75421234c1025eab65c1a983096ae2d23c4e0
root <root@DESKTOP-RDFI041>
2024-03-24 10:29:38 +0530

    new commit march24

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -16,3 +16,11 @@ resource "aws_vpc" "demovpc" {
   cidr_block = var.vpc_cidr_block
   tags      = var.vpc_tags
 }
+# Creating 1st web subnet
+resource "aws_subnet" "public-subnet-1" {
+  vpc_id                  = aws_vpc.demovpc.id
+  cidr_block              = var.public_subnet_cidr
+  map_public_ip_on_launch = true
+  availability_zone       = var.public_subnet_az
+  tags                    = var.public_subnet_tags
+}
