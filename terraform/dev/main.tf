93cda0833bae55be88b785a9fefb4803c8e758a4
root <root@DESKTOP-RDFI041>
2024-04-17 09:27:50 +0530

    new commit17

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -11,16 +11,7 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-# vpc creation
-resource "aws_vpc" "demovpc" {
-  cidr_block = var.vpc_cidr_block
-  tags      = var.vpc_tags
-}
-# Creating 1st web subnet
-resource "aws_subnet" "public-subnet-1" {
-  vpc_id                  = aws_vpc.demovpc.id
-  cidr_block              = var.public_subnet_cidr
-  map_public_ip_on_launch = true
-  availability_zone       = var.public_subnet_az
-  tags                    = var.public_subnet_tags
+# s3 bucket creation
+resource "aws_s3_bucket" "onebuck1" {
+  bucket = var.buck_name
 }
