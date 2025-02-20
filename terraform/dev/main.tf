cdd555fba9d9035a43231a700c69cfdebccafb54
root <root@DESKTOP-RDFI041>
2025-02-20 11:40:56 +0530

    commit20feb

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -24,17 +24,4 @@ resource "aws_instance" "WEBinstance" {
   ]
 }
 
-resource "tls_private_key" "pk" {
-  algorithm = "RSA"
-  rsa_bits  = var.rsa_bits
-}
-
-resource "aws_key_pair" "kp" {
-  key_name   = var.key_name
-  public_key = tls_private_key.pk.public_key_openssh
-
-  provisioner "local-exec" {
-    command = "echo '${tls_private_key.pk.private_key_pem}' > ./myKey.pem"
-  }
-}
 
