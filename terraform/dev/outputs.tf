d800aa9d571fba049ce03379f3f125a80dd4b1c4
root <root@DESKTOP-RDFI041.localdomain>
2025-11-01 12:32:44 +0530

    1nov25

--- terraform/dev/outputs.tf
+++ terraform/dev/outputs.tf
@@ -1,3 +1,20 @@
- output "public-ip-address" {
-  value = aws_instance.example.public_ip
+output "vpc_id" {
+  description = "The ID of the created VPC"
+  value       = aws_vpc.demovpc.id
 }
+
+output "vpc_cidr_block" {
+  description = "The CIDR block of the created VPC"
+  value       = aws_vpc.demovpc.cidr_block
+}
+
+output "vpc_arn" {
+  description = "The ARN of the created VPC"
+  value       = aws_vpc.demovpc.arn
+}
+
+output "vpc_tags" {
+  description = "The tags assigned to the VPC"
+  value       = aws_vpc.demovpc.tags
+}
+
